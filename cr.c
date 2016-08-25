d57f1b958184847fd4101c7905462de05a5c34a0
Chris Laverdiere <cmlaverdiere@gmail.com>
2016-08-24 21:06:10 -0400

    Add parameter to specify named process

--- cr.c
+++ cr.c
@@ -6,56 +6,71 @@
 // If no command is supplied, it pings the running instance.
 // Handy for quick compile + run keybindings, or anything repetitive.
 //
-// Not sure if there's already an existing program for this purpose, but hey
-// it's only 50 lines.
+// Notes: Handles at most 26 processes. One for each letter id.
+//
+// Examples:
+// cr r 'make && ./run'
+// cr t 'make tests'
 
+#include <ctype.h>
 #include <errno.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 int main(int argc, char* argv[])
 {
-    char* fifo_path = "/tmp/crfifo";
     char buf[1];
     int fd;
 
-    // If arguments given, start a new process.
-    if (argc == 2) {
-        printf("Command: %s\n", argv[1]);
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "Usage: cr <id> <cmd>\n");
+        exit(EXIT_FAILURE);
+    }
 
-        /* puts("Creating cr pipe"); */
+    char id = tolower(argv[1][0]);
+    if (!isalpha(id)) {
+        fprintf(stderr, "Bad id %c given: id must be alphabetical.\n", id);
+        exit(EXIT_FAILURE);
+    }
+
+    char* fifo_path;
+    sprintf(fifo_path, "/tmp/crfifo_%c", id);
+
+    // If a command argument is given, start a new process.
+    if (argc == 3) {
+        char* cmd = argv[2];
+
+        printf("Running command (on %c): %s\n", id, cmd);
         mkfifo(fifo_path, 0666);
 
         while(1) {
             fd = open(fifo_path, O_RDONLY);
 
             if (fd == -1) {
-                printf("%s\n", strerror(errno));
+                fprintf(stderr, "%s\n", strerror(errno));
                 exit(EXIT_FAILURE);
             }
 
-            /* puts("Reading cr pipe"); */
             read(fd, buf, 1);
-            system(argv[1]);
+            system(cmd);
         }
 
         close(fd);
 
-    } else { // No arguments given, just ping process.
+    } else { // No command argument given, just ping process <id>.
         fd = open(fifo_path, O_WRONLY);
 
         if (fd == -1) {
-            printf("%s\n", strerror(errno));
+            fprintf(stderr, "%s\n", strerror(errno));
             exit(EXIT_FAILURE);
         }
 
-        /* puts("Writing to cr pipe"); */
-        buf[0] = 'b';
+        buf[0] = id;
         write(fd, buf, 1);
         close(fd);
     }
